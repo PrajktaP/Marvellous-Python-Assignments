fbf1fa3c9bf36d2f6685bba8dd9f5113a7f4e193
prajkta <prajkta@techelogy.com>
2025-07-20 16:20:26 +0530

    remaining questions from assignment 25

--- Assignment_25/Assignment25_7.py
+++ Assignment_25/Assignment25_7.py
@@ -1,5 +1,33 @@
+import pandas as pd
+
+def create_dataframe():
+    line = "*"*100
+
+    data = {'Age': [18, 22, 25, 30, 35]} 
+
+    print(line)
+
+    # create Dataframe
+    df = pd.DataFrame(data)
+
+    print(f"Created DataFrame: \n{line}\n{df}\n")
+
+    return df
+
+def normalize_column(df, column):
+    line = "*"*100
+
+    min = df[column].min()
+    max = df[column].max()
+    df[column] = (df[column] - min) / (max - min)
+
+    # create Dataframe
+    print(line)
+    print(f"Normalized DataFrame: \n{line}\n{df}\n")
+
 def main():
-    pass
+    df = create_dataframe()
+    normalize_column(df, 'Age')
 
 if __name__ == "__main__":
     main()
--- Assignment_25/Assignment25_9.py
+++ Assignment_25/Assignment25_9.py
@@ -1,5 +1,30 @@
+import pandas as pd
+import numpy as np
+
+def create_dataframe():
+    line = "*"*100
+
+    data = {'Marks': [45, 67, 88, 32, 76]} 
+
+    print(line)
+
+    # create Dataframe
+    df = pd.DataFrame(data)
+    print(f"Created DataFrame with missing data: \n{line}\n{df}\n")
+
+    return df
+
+def replace_values(df):
+    line = "*"*100
+
+    df['Marks'] = df['Marks'].where(df['Marks'] >= 50, 'Fail')
+    
+    print(f"DataFrame after filling the missing data: \n{line}\n{df}\n")
+
 def main():
-    pass
+    df = create_dataframe()
+    replace_values(df)
+
 
 if __name__ == "__main__":
     main()
